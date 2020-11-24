a0b43f388c370b42f0208f325247946e9315e60f
Chetan2028 <bmchetan2028@gmail.com>
2020-11-24 19:05:54 +0530

    Add project files.

---
+++ EmployeePayRollServiceDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmployeePayRollServiceDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
