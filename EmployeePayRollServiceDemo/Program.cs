1e67e27435869cfa25c6dd64028c75bf711bec0b
Chetan2028 <bmchetan2028@gmail.com>
2020-11-24 23:05:22 +0530

    [Chetan] Add . Added Use cases for normalized table

--- EmployeePayRollServiceDemo/Program.cs
+++ EmployeePayRollServiceDemo/Program.cs
@@ -9,7 +9,12 @@ namespace EmployeePayRollServiceDemo
             Console.WriteLine("Welcome to Employee Payroll Database");
             EmployeeRepo employeeRepo = new EmployeeRepo();
             employeeRepo.EnsureDatabaseConnection();
-            employeeRepo.GetAggregateFunctions();
+            //Console.WriteLine("---------------------------------------------RETRIEVE DATA--------------------------------------------------------");
+            //employeeRepo.GetAllEmployees_NewTableStructure();
+            //Console.WriteLine("---------------------------------------------AGGREGATE FUNCTIONS--------------------------------------------------------");
+            //employeeRepo.GetAggregateSalaryForEachGender_NewTable();
+            Console.WriteLine("---------------------------------------------EMPLOYEES WIHTIN GIVEN DATE RANGE--------------------------------------------------------");
+            employeeRepo.GetEmployeesWithinGivenDateRange_NewTable();
         }
     }
 }
